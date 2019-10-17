9e39f8ccc95752777baa105860130311869e9bff
EdZa <edalzap@gmail.com>
2019-10-16 23:45:00 -0500

    fix 1

--- Ex03_Dag/limpiar.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Guardar argumento 1
-fIn=$1
-# Quitar la extension .txt del nombre de archivo
-fName=${fIn%\.txt}
-fOut=${fName}C.txt
-# Eliminar URLs repetidas y guardar resultado en fOut
-sort -n ${fIn} | uniq > ${fOut}
--- Ex03_Dag/prepara.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Colocar archivos de entrada en las carpetas esperadas por el Job.
-for((i=0;i<4;++i));
- do
-  mkdir run_${i}
-  cp checkBM.py urlsC_${i}.txt run_${i}
- done
